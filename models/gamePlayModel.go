e52f35c892fd96f2447cf7e1ef38e8ac138c7bf6
Chayapol Wongpuwarak <kongkcypw@gmail.com>
2024-07-07 20:30:43 +0700

    update: game play model

--- models/gamePlayModel.go
+++ models/gamePlayModel.go
@@ -4,4 +4,6 @@ type GamePlayLevel struct {
 	LevelId     string `bson:"_id" json:"id"`
 	LevelNumber int64  `bson:"levelNumber" json:"levelNumber"`
 	LevelName   string `bson:"levelName" json:"levelName"`
+	Description string `bson:"description" json:"description"`
+	IsActive    bool   `bson:"isActive" json:"isActive"`
 }
